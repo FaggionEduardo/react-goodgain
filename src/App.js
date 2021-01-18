210030a003ef36a521d0e1a16d72128af1ec4d62
Eduardo Faggion <>
2021-01-18 17:20:35 -0300

    add tag manager

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 
 import Routes from "./routes";
 
+import TagManager from 'react-gtm-module' 
+
+const tagManagerArgs = { 
+    gtmId: 'GTM-KCX9564' 
+} 
+
+TagManager.initialize (tagManagerArgs)
+
 const App = () => <Routes />;
 
 export default App;
